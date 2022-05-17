d058cda57815def41db9ef51314e99b825d9963a
Jeanmichel7 <jeanmichel.rasser@laposte.net>
2022-05-17 06:59:42 +0200

    header

--- includes/mini_shell.h
+++ includes/mini_shell.h
@@ -6,7 +6,7 @@
 /*   By: jrasser <jrasser@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 05:27:48 by jrasser           #+#    #+#             */
-/*   Updated: 2022/05/17 05:40:46 by jrasser          ###   ########.fr       */
+/*   Updated: 2022/05/17 06:58:44 by jrasser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,18 @@
 # include "../libft/libft.h"
 # include <stdlib.h>
 # include <stdio.h>
-/* # include <readline/readline.h> */
-/* # include <readline/history.h> */
+# include <unistd.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <sys/resource.h>
+# include <signal.h>
+# include <dirent.h>
+# include <string.h>
+# include <sys/ioctl.h>
+# include <termios.h>
 
 #endif
