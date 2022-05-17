bfa7a9d058100396dad6f98b18dfaf488531581a
Jeanmichel7 <jeanmichel.rasser@laposte.net>
2022-05-17 13:43:58 +0200

    struct

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   mini_shell.h                                       :+:      :+:    :+:   */
+/*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jrasser <jrasser@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 05:27:48 by jrasser           #+#    #+#             */
-/*   Updated: 2022/05/17 10:15:58 by jrasser          ###   ########.fr       */
+/*   Updated: 2022/05/17 13:43:31 by jrasser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINI_SHELL_H
-# define MINI_SHELL_H
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
 # include "../libft/libft.h"
 # include <stdlib.h>
@@ -30,4 +30,29 @@
 # include <sys/ioctl.h>
 # include <termios.h>
 
+/*
+exemple de char *cmds (tout le contenu entre 2 pipes)
+[0] : "ls"
+[1] : "-arth"
+[2] : ">"
+[3] : "fichier"
+*/
+
+typedef struct s_input
+{
+	int		redir_input;
+	int		redir_output;
+	int		redir_double_input;
+	int		redir_double_output;
+	char	*cmds;
+}	t_input;
+
+typedef struct s_inputs
+{
+	int		nb_pipe;
+	t_input	*input;  // => tableau d'input (genre 3pipe => 4 inputs différents)
+}	t_inputs;
+
+/* si t'as une meilleur idée pour le nom des structures inputs / input (c'est un peu confu ou ca va ?) 
+ou d'une autre strucutre merci de me contacter au 3615 */
 #endif
