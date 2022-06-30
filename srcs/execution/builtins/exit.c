58abc02cbd9aebba013c5b8cb2b6f6c633427d74
Jean-michel Rasser <jrasser@k0r3p4.42mulhouse.fr>
2022-06-30 18:03:58 +0200

    error exit

--- srcs/execution/builtins/exit.c
+++ srcs/execution/builtins/exit.c
@@ -6,32 +6,11 @@
 /*   By: jrasser <jrasser@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 02:51:43 by jrasser           #+#    #+#             */
-/*   Updated: 2022/06/30 17:17:18 by jrasser          ###   ########.fr       */
+/*   Updated: 2022/06/30 18:03:41 by jrasser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-
-void	ft_sub_exit(int code, char *str_code)
-{
-	char				*code_ptr;
-	long unsigned int	j;
-
-	code_ptr = ft_putunbr_base(code, "0123456789abcdef");
-	write(1, "[processus terminé avec le code ", 33);
-	write(1, str_code, ft_strlen(str_code));
-	write(1, " (0x", 5);
-	j = 0;
-	while (j < 8 - ft_strlen(str_code))
-	{
-		write(1, "0", 1);
-		j++;
-	}
-	write(1, code_ptr, ft_strlen(code_ptr));
-	write(1, ")]\n", 4);
-	g_error_code = code;
-	free(code_ptr);
-}
 
 int	ft_check_num(char *str)
 {
@@ -75,6 +54,15 @@ int	ft_check_exit_error(t_data *data, int i)
 	return (1);
 }
 
+int	ft_sub_exit(char *str_code)
+{
+	int	code;
+
+	code = ft_atoi(str_code);
+	g_error_code = code;
+	return (code);
+}
+
 void	ft_exit(t_data *data, int i)
 {
 	char	*str_code;
@@ -87,17 +75,17 @@ void	ft_exit(t_data *data, int i)
 		{
 			str_code = data->inputs[i].cmds[1];
 			if (ft_check_num(str_code))
-				code = ft_atoi(str_code);
+				ft_sub_exit(str_code);
 			else
 			{
 				str_code[0] = '2';
 				str_code[1] = '\0';
 			}
-			ft_sub_exit(code, str_code);
 		}
 		if (data->nb_pipe == 0)
 		{
 			write(1, "exit\n", 5);
+			tcsetattr(0, 0, &(data->termios_save));
 			data->done = 1;
 		}
 	}
